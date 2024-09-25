7ecc859f002a85702a292487441aeb2777bcced1
C.Gunasekar <137645060+guna-design@users.noreply.github.com>
2024-09-25 21:40:09 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://chamberoflearning.com',
+  base:'https://chamberoflearning.com/non-prod/React',
   plugins: [react()],
   resolve:{
    alias: [{find:"@", replacement:"/src"}],
